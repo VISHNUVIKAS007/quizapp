314d4d49f683d5ba2dd60f2121b23fa2ed28efbc
vishnu <vishnu@example.com>
2019-09-16 00:41:44 +0530

    fifth

--- lib/main.dart
+++ lib/main.dart
@@ -4,51 +4,63 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  void answerQuestion(){
-   print('Answer is answer');
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
+
+}
+class MyAppState extends State<MyApp>{
+  var questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    var questions =[
+    var questions = [
       'what\'s your favorite Color?',
       'what\'s your favorite Animal?',
     ];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.redAccent,
-          title: Text(
-            'Quiz App',
-            textAlign: TextAlign.center,
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.redAccent,
+            title: Text(
+              'Quiz App',
+              textAlign: TextAlign.center,
+            ),
+            elevation: 1.0,
           ),
-          elevation: 1.0,
-        ),
-        body: Column(
-          children: <Widget>[
-            Text(questions.elementAt(0),),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 4'),
-              onPressed: answerQuestion,
-            ),
-
-          ],
-        )
-      ),
+          body: Column(
+            children: <Widget>[
+              Text(
+                questions[questionIndex],
+              ),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 4'),
+                onPressed: answerQuestion,
+              ),
+            ],
+          )),
     );
   }
 }
